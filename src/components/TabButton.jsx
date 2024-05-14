1c6162ef2a7feab26bdebebb42ef1d4aa27e7aee
ankurjbs <ankurjbs@gmail.com>
2024-05-14 12:38:12 +0530

    testing git private and ublic

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,6 +1,6 @@
 export default function TabButton(props) {
     function handleClick(){
         alert('hello');
-    }
+    } 
   return <li><button onClick={handleClick} >{props.children}</button></li>;
 }
